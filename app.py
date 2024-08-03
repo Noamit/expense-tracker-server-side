85b080cb285628eca01ca76671b5fe3ebab1d45b
noamit <noa1amit2@gmail.com>
2024-08-03 16:41:16 +0300

    init app configuration

--- app.py
+++ app.py
@@ -1,3 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify
+from db import db
+from dotenv import load_dotenv
+from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+import models
+
+from resources.user import blp as UserBlueprint
+
+
+def create_app(db_url=None):
+
+    app = Flask(__name__)
+    load_dotenv()
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    # will create a file called data.db.
+    # if there is db_url it will connect to the uel and if not it will conect to the env variable by sqlite
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+
+    # init SQLALCHEMY with our flack app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    api = Api(app)
+
+    # todo: create a new key and remove from code
+    app.config["JWT_SECRET_KEY"] = "76517074903035659443580166051695395839"
+    jwt = JWTManager(app)
+
+    api.register_blueprint(UserBlueprint)
+    return app
