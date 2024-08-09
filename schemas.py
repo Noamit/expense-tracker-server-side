c3cc397d0620e6c7382676cd9e0de194178bb545
noamit <noa1amit2@gmail.com>
2024-08-09 12:52:31 +0300

    user and expense scheme for the current endpoints

--- schemas.py
+++ schemas.py
@@ -1,7 +1,23 @@
 from marshmallow import Schema, fields
 
 
-class UserSchema(Schema):
+class PlainExpenseSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    description = fields.Str()
+    amount = fields.Float(required=True)
+    date = fields.Date(required=True)
+
+
+class PlainUserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True, load_only=True)
+
+
+class ExpenseSchema(PlainExpenseSchema):
+    user = fields.Nested(PlainUserSchema(), dump_only=True)
+
+
+class UserSchema(PlainUserSchema):
+    items = fields.List(fields.Nested(PlainExpenseSchema()), dump_only=True)
