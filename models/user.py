3fa00dd87e0afeb6c7ddeaa0dbd68c0bc3d3904e
noamit <noa1amit2@gmail.com>
2024-08-09 12:51:53 +0300

    update relation between expense and user

--- models/user.py
+++ models/user.py
@@ -7,3 +7,6 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+
+    expenses = db.relationship(
+        "ExpenseModel", back_populates="user", lazy="dynamic", cascade="all, delete")
